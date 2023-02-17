f488e466f0bf68aa55f5274e0878e399fc9f2e14
UltramicroscopicWIMP <BLAISEMBUCKSEK@GMAIL.COM>
2023-02-17 09:06:35 -0800

    update all 4-4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
-/*
- *@main
- *returns integer on exit
- *@param void
- *puts a string of char to standard input
+/**
+ *main - puts a string of char to standard input
+ *@param void: doesn't take any parameter
+ *return: int. 0 on quitting
  */
 int main(void)
 {
@@ -11,9 +10,3 @@ char textmsg[] = "\"Programming is like building a multilingual puzzle";
 puts(textmsg);
 return (0);
 }
-/*                                                                                                                                          
- *@main                                                                                                                                     
- *returns integer on exit                                                                                                                   
- *@param void                                                                                                                               
- *puts a string of char to standard input                                                                                                   
- */

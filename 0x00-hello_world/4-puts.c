4043eb2ce2498f4a5b9d6f5e48e1430dd1efd201
UltramicroscopicWIMP <BLAISEMBUCKSEK@GMAIL.COM>
2023-02-17 08:47:36 -0800

    update all 4-2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include<stdio.h>
+/*
+ *@main
+ *returns integer on exit
+ *@param void
+ *puts a string of char to standard input
+ */
 int main(void)
 {
 char textmsg[] = "\"Programming is like building a multilingual puzzle";

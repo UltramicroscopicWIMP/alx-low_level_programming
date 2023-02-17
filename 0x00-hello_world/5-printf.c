f306c8241d3931f25e1cf334409be2e1bb788375
UltramicroscopicWIMP <BLAISEMBUCKSEK@GMAIL.COM>
2023-02-17 09:21:04 -0800

    update all 5-1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
-int main()
-{ printf("with proper grammar, but the outcome is a piece of art,\n");
-  return 0;
+/**
+ *main - prints to standard output a string text with newline
+ *
+ *Return: int. returns 0 before exiting
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }

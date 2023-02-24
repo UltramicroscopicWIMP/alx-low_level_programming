aa363dacbefdb14506e9867397ae419a560b40a3
UltramicroscopicWIMP <BLAISEMBUCKSEK@GMAIL.COM>
2023-02-23 19:09:26 -0800

    2 multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+int mul(int a, int b)
+{
+int val = a*b;
+return (val);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef _main_h
 #define _main_h
 int _isdigit(int);
+int mul(int, int);
 int _isupper(int);
 #endif

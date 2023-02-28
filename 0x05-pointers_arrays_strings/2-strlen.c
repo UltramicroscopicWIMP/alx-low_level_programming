e17a879b00df7b59e0ad88b6cda36105b4122f28
UltramicroscopicWIMP <BLAISEMBUCKSEK@GMAIL.COM>
2023-02-28 09:13:49 -0800

    2 update 0

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_strlen - counts and returns the number of chars in a string
+*@s: an array of chars
+*
+*Return: int. Returns the number of chars in the string
+*/
+int _strlen(char *s)
+{
+	char c = *s;
+	int count = 0;
+	while (c != '\0')
+	{
+		count++;
+		c = *(s+count);
+	}
+	return (count);
+}

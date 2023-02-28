efbfc4d884ddf80f39da41e343271c6febbd7a10
UltramicroscopicWIMP <BLAISEMBUCKSEK@GMAIL.COM>
2023-02-28 09:01:38 -0800

    0 update 2

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,11 +2,11 @@
 
 /**
 *reset_to_98- changes value pointed by n to 98
+*@param:int *n, a pointer to an integer
 *
 *Return: void. returns 0.
 */
 void reset_to_98(int *n)
 {
 	*n = 98;
-	return;
 }

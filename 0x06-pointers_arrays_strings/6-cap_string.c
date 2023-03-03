0843753b724c1d5fb25492f5e04e674d12b23be6
UltramicroscopicWIMP <BLAISEMBUCKSEK@GMAIL.COM>
2023-03-03 12:00:29 -0800

    update 7 #2

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,44 +1,47 @@
 #include "main.h"
 
 /**
- *cap_string - capitalizes all words of a string
- *@str: An array of chars
- *
- *Return: An array of chars
- */
+*cap_string - capitalizes all words of a string
+*@str: An array of chars
+*
+*Return: An array of chars
+*/
 char *cap_string(char *str)
 {
-  char sep[] = {' ', '\t', '\n', ',' , ';', '.', '!', '?', '"', '(', ')', '{', '}', '\0'};
-  char c = *str;
-  char s = sep[0];
-  int count_sep = 0;
-  int i = 0;
-  int k;
-  int count = 0;
+	char sep[] = {' ', '\t', '\n', ',' , ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	char c = *str;
+	char s = sep[0];
+	int count_sep = 0;
+	int i = 0;
+	int k;
+	int count = 0;
 
-  while (c != '\0')
-    {
-      count++;
-      c = *(str + count);
-    }
-  while ( s != '\0')
-    {
-      count_sep++;
-      s = sep[count_sep];
-    }
-  while (i < count)
-    {
-      k = 0;
-      while (k < count_sep)
+	while (c != '\0')
 	{
-	  if (str[i] == sep[k] && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
-	    {
-	      str[i + 1] = str[i + 1] + 'A' - 'a';
-	    }
-	  k++;
+		count++;
+		c = *(str + count);
 	}
-      i++;
-    }
-  return (str);
+	while ( s != '\0')
+	{
+		count_sep++;
+		s = sep[count_sep];
+	}
+	while (i < count)
+	{
+		k = 0;
+		while (k < count_sep)
+		{
+			if (str[i] == sep[k] && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
+			{
+				if (sep[k] == '\t')
+				{
+					str[i] = sep[0];
+				}
+				str[i + 1] = str[i + 1] + 'A' - 'a';
+			}
+			k++;
+		}
+		i++;
+	}
+	return (str);
 }
-

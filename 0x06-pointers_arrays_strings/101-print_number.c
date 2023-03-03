856958355d173125528b2f44fb5ff110c63412c9
UltramicroscopicWIMP <BLAISEMBUCKSEK@GMAIL.COM>
2023-03-03 15:48:45 -0800

    print 9 #0

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*print_number - prints an integer
+*@n: integer to be printed
+*
+*Return: Void. Returns nothing
+*/
+void print_number(int n)
+{
+	_putchar('\0' + n);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef _main_h
 #define _main_h
 #include <stdio.h>
+int _putchar(char);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);

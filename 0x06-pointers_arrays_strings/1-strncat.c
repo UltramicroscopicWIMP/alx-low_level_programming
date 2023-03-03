e3cbe5095c83ac66f57bcc8924a8be6e88941df8
UltramicroscopicWIMP <BLAISEMBUCKSEK@GMAIL.COM>
2023-03-03 05:42:40 -0800

    update all 4

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
-*_strncat - catenates n chars from a string to another full string
-*@dest: The string onto which contenation occurs
-*@src: The string from which n chars are pulled from
-*@n: The number of integers to be pulled from src
+*_strncat- catenates n chars from a string to another full string
+*@dest: An array of chars. The string onto which contenation occurs
+*@src: Array of chars. The string from which n chars are pulled from
+*@n: int. The number of integers to be pulled from src
 *
-*Return: char Poiner.  A pointer to the resulting concatenation.
+*Return: char*.  A pointer to the resulting concatenation.
 */
 char *_strncat (char *dest, char *src, int n)
 {
 	int count = 0;
 	char c = *dest;
-	char dest_prime[100];
 	int i = 0;
 
 	while (c != '\0')
@@ -22,10 +21,8 @@ char *_strncat (char *dest, char *src, int n)
 	}
 	while (i < n)
 	{
-		dest_prime[count + i] = src[i];
+		dest[count + i] = src[i];
 		i++;
 	}
-	dest = dest_prime;
-	return (dest);
+	return(dest);
 }
-

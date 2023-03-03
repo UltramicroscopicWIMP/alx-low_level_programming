a4a098f11130d8335b5b5a0e966317c6ceb39724
UltramicroscopicWIMP <BLAISEMBUCKSEK@GMAIL.COM>
2023-03-03 07:13:43 -0800

    update all 14

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,35 +1,35 @@
 #include "main.h"
 
 /**
- *_strcat - takes two strings and concatenates them
- *@dest: Pointer to the result of the concatenation
- *@src: Pointer to the string that must be concatenated with dest
- *
- *Return: char array, A pointer to the resulting string dest
- */
+*_strcat - takes two strings and concatenates them
+*@dest: Pointer to the result of the concatenation
+*@src: Pointer to the string that must be concatenated with dest
+*
+*Return: char array, A pointer to the resulting string dest
+*/
 char *_strcat(char *dest, char *src)
 {
-  char c = *dest;
-  int count_d = 0;
-  int count_s = 0;
-  int i;
+	char c = *dest;
+	int count_d = 0;
+	int count_s = 0;
+	int i;
 
-  while (c != '\0')
-    {
-      count_d++;
-      c = *(dest + count_d);
-    }
-  c = *src;
-  while (c != '\0')
-    {
-      count_s++;
-      c = *(src + count_s);
-    }
-  for (i = 0; i < count_s; i++)
-    {
-      dest[i + count_d] = src[i];
-    }
+	while (c != '\0')
+	{
+		count_d++;
+		c = *(dest + count_d); 
+	}
+	c = *src;
+	while (c != '\0')
+	{
+		count_s++;
+		c = *(src + count_s);
+	}
+	for (i = 0; i < count_s; i++)
+	{
+		*(dest + i + count_d) = *(src + i);
+	}
 
-  dest[count_d + count_s] = '\0';
-  return (dest);
+	*(dest + count_d + count_s) = '\0';
+	return (dest);
 }

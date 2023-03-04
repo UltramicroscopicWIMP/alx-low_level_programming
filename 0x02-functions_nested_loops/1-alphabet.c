72d3bb7f8684f106abc61cb3d9dca81f052a95f7
UltramicroscopicWIMP <BLAISEMBUCKSEK@GMAIL.COM>
2023-03-04 01:37:10 -0800

    fix betty coding style erorrs in 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,18 @@
 #include "main.h"
 
-void print_alphabet(void){
-int i = 0;
-while (i < 26){
-_putchar(97+i);
-i++;
-}
- _putchar('\n');
-return;
+/**
+*print_alphabet - prints the alphabet in lowercase followed by the newline
+*
+*Return: Void. returns nothing
+*/
+void print_alphabet(void)
+{
+	int i = 0;
+
+	while (i < 26)
+	{
+		_putchar(97+i);
+		i++;
+	}
+	 _putchar('\n');
 }

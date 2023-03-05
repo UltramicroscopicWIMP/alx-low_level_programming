3cdf503b15d2046a4eedd5737341544a5e6e8d44
UltramicroscopicWIMP <BLAISEMBUCKSEK@GMAIL.COM>
2023-03-05 15:47:40 -0800

    fix is digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,19 @@
 #include "main.h"
 
+/**
+ *_isdigit - checks if c is a digit
+ *@n: the value to be checked
+ *
+ *Return: int. returns 1 if n is a digit and 0 oherwise
+ */
 int _isdigit(int c)
 {
-if (c >= 48 && c < 58)
-{
-return (1);
-}
-else
-{
-return (0);
-}
+	if (c >= 0 && c < 10)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }

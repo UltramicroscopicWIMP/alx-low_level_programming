0623d219c5c708002cce8530f99d6d1f42c1c22b
UltramicroscopicWIMP <BLAISEMBUCKSEK@GMAIL.COM>
2023-03-05 15:56:59 -0800

    fix betty issues in 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,7 +1,15 @@
 #include "main.h"
 
+/**
+*mul - multiplies two numbers
+*@a: the first of two numbers
+*@b: the second of two numbers
+*
+*Return: int. the product of a and b
+*/
 int mul(int a, int b)
 {
-int val = a*b;
-return (val);
+	int val = a*b;
+	return (val);
 }
+

96c81327c845b16e7dd69c8c6d6a0098fee68eae
UltramicroscopicWIMP <BLAISEMBUCKSEK@GMAIL.COM>
2023-03-05 08:12:04 -0800

    add n1 and n2

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+*add - adds two integers
+*@n1: the first integer to be added
+*@n2: the second of two integers
+*
+*Return: int. the sum of the two integers
+*/
+int add(int n1, int n2)
+{
+	int sum;
+	sum = n1 + n2;
+
+	return (sum);
+}

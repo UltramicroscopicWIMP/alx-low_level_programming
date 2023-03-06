fcab44dab4ceda61b70d9e3d15e39cea4de1889f
UltramicroscopicWIMP <BLAISEMBUCKSEK@GMAIL.COM>
2023-03-05 16:11:57 -0800

    fix betty issues with print numbers 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,19 @@
 #include "main.h"
 
+/**
+*print_numbers - print numbers from 0 to 9 followed by \n
+*
+*Return: Void. returns nothing
+*/
 void print_numbers(void)
 {
-int i = 0;
-while (i < 10)
-{
-_putchar('0'+i);
-i++;
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar('0' + i);
+		i++;
+	}
+	_putchar('\n');
 }
-_putchar('\n');
-}
+

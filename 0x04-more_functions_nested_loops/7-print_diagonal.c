a8045283a22ab4014f92f353e4c18966ee43ddee
UltramicroscopicWIMP <BLAISEMBUCKSEK@GMAIL.COM>
2023-03-05 16:47:36 -0800

    betty kind of problems with print diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,21 +1,27 @@
 #include "main.h"
 
+/**print_diagonal - prints the diagonal on term
+*@n: the number of diagonals
+*
+*Return: void. returns nothing
+*/
 void print_diagonal(int n)
 {
-if(n <= 0)
-{
-_putchar('\n');
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		if (1 < n)
+		{
+			_putchar(' ');
+			print_diagonal(n - 1);
+		}
+		else
+		{
+			_putchar('\\');
+		}
+	}
 }
-else
-{
-if (1 < n)
-{
-_putchar(' ');
-print_diagonal(n-1);
-}
-else
-{
-_putchar('\');
-}
-}
-}
+

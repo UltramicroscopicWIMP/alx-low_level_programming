686d6900a9fcbf822a61977078997a02adf25abd
UltramicroscopicWIMP <BLAISEMBUCKSEK@GMAIL.COM>
2023-03-05 16:26:00 -0800

    fix more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,23 +1,24 @@
 #include "main.h"
-#include <stdio.h>
 
-int __print(int n)
-{
-  return (n);
-}
+/**
+*more_numbers - prints all numbers from 0 to 14 ten times
+*
+*Return: Void. Returns nothing
+*/
 void more_numbers(void)
 {
-int i = 0;
-int j;
-while (i < 10)
-{
-j = 0;
-while (j < 15)
-{
-__print(j);
-j++;
-}
-_putchar('\n');
-i++;
-}
+	int i = 0;
+	int j;
+
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 15)
+		{
+			_putchar('0' + j);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }

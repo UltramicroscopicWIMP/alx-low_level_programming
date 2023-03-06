e11a57c0b8e8c4eec8efb8ebdc5a3a2b0d61b59b
UltramicroscopicWIMP <BLAISEMBUCKSEK@GMAIL.COM>
2023-03-05 16:30:16 -0800

    fix bety isues in 6 print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,14 +1,20 @@
 #include "main.h"
 
+/**
+*print_line - print lines using '-' character
+*@n: the number of lines
+*
+*Return: void. return nothing
+*/
 void print_line(int n)
 {
-if (n <= 0)
-{
-_putchar('\n');
-}
-else
-{
-_putchar('_');
-print_line(n-1);
-}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('_');
+		print_line(n - 1);
+	}
 }

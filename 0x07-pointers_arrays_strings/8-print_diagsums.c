f260f20dfd630bce2a55dd68d40b3c588548c185
UltramicroscopicWIMP <BLAISEMBUCKSEK@GMAIL.COM>
2023-03-07 15:35:13 -0800

    print the sum  of the leading and off diagonals

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*print_diagsums - prints the sum of the two diagonals of a square matrix of ints
+*@a: an array of ints
+*@size: int. the size of the square matrix
+*
+*Return: void. returns nothing
+*/
+void print_diagsums(int *a, int size)
+{
+	int leading_sum = 0;
+	int off_sum = 0;
+	int i = 0;
+
+	while (i < size)
+	{
+		leading_sum = leading_sum + a[i][i];
+		off_sum = off_sum + a[i][size - i - 1];
+		i++;
+	}
+	printf("%d, %d", leading_sum, off_sum);
+}

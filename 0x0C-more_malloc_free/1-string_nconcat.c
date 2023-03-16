68dae4e71c41286a9e744dec4ed6b82b0ccba8eb
UltramicroscopicWIMP <BLAISEMBUCKSEK@GMAIL.COM>
2023-03-16 15:30:37 -0700

    consider s1 and or s2 as an empty string when null is passed

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,6 +15,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *str;
 	unsigned int i = 0;
 
+	if (s1 == NULL)
+	{
+	        s1 = "";
+	}
+	if (s2 == NULL)
+	{
+	        s2 = "";
+	}
 	if (n >= strlen(s2))
 	{
 		n = strlen(s2);

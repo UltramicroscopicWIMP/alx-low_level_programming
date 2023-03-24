f223e52c3067ee322cfafc52835bd392f7a4f221
UltramicroscopicWIMP <BLAISEMBUCKSEK@GMAIL.COM>
2023-03-23 19:41:22 -0700

    Add header files of std c to variadic_functions.h from 3-print_all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,9 @@
 #ifndef _variadic_functions_h
 #define _variadic_functions_h
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);

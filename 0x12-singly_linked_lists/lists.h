fa609822182ad422e71804bab29cb59e895a9dcb
UltramicroscopicWIMP <BLAISEMBUCKSEK@GMAIL.COM>
2023-04-03 07:59:40 -0700

    Add a semi-colon to the print_list prototype function in lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -17,9 +17,9 @@ typedef struct list_s
     unsigned int len;
     struct list_s *next;
 } list_t;
-size_t list_len(const list_t *h);
-size_t print_list(const list_t *h)
-list_t *add_node(list_t **head, const char *str_);
-list_t *add_node_end(list_t **head, const char *str_);
-void free_list(list_t *head);
+size_t list_len(const list_t*);
+size_t print_list(const list_t*);
+list_t *add_node(list_t**, const char*);
+list_t *add_node_end(list_t**, const char*);
+void free_list(list_t*);
 #endif

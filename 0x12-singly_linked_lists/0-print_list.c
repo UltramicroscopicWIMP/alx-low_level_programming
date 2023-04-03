1f9692b3b9a4aa78753ffcf1a260f07f4517ed2c
UltramicroscopicWIMP <BLAISEMBUCKSEK@GMAIL.COM>
2023-04-03 08:23:42 -0700

    Put the brace after else statement on the next line for formatting

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,13 +12,14 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-	  count++;
+		count++;
 		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
-		else{
-		printf("[%d] %s\n", h->len, h->str);
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
 		}
 		h = h->next;
 	}

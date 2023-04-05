7605b4720447d18a2c1c5b82b94c58f7ba195d93
UltramicroscopicWIMP <BLAISEMBUCKSEK@GMAIL.COM>
2023-04-04 17:16:57 -0700

    Add 6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+*pop_listint - deletes the first node
+*@head: The address of the pointer to the first node
+*
+*Return: Integer. Returns the value stored at the first node
+*/
+int pop_listint(listint_t **head)
+{
+	int val;
+	listin_t *prev;
+
+	val = 0;
+	if (*head != NULL)
+	{
+		prev = *head;
+		val = prev->n;
+		*head = (*head)->next;
+		free(prev);
+		prev = NULL;
+	}
+	return val;
+}
